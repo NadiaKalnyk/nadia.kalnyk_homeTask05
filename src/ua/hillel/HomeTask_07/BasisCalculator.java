9edded771746ee5f01ddd8433f505a8b2ff20ef7
NadiaKalnyk <kalnuk.2016.nadiya@gmail.com>
2022-11-30 01:20:08 +0200

    changes for calculator

--- src/ua/hillel/HomeTask_07/BasisCalculator.java
+++ src/ua/hillel/HomeTask_07/BasisCalculator.java
@@ -1,59 +1,42 @@
 package ua.hillel.HomeTask_07;
 
 public class BasisCalculator{
-
+int number1;
+int number2;
     public static void getSum(int number1, int number2) {
-        System.out.println(number1+number2);
+        int sum = number1+number2;
+        System.out.println("Basis Sum= " + sum);
     }
 
     public static void getDifference(int number1, int number2) {
-        System.out.println(number1-number2);
+        int diff = number1-number2;
+        System.out.println("Basis Difference= " + diff);
     }
 
     public static void getMultiplication(int number1, int number2) {
-        System.out.println(number1 * number2);
+        int multiplication = number1 * number2;
+        System.out.println("Basis Multiplication= " + multiplication);
     }
-
-    public static void getDivision (String str, int number1, int number2) {
+    public static void getDivision (int number1, int number2) {
         if (number2 == 0) {
-            System.out.println("error.Division by zero" + str);
+            System.out.println("ERROR.Division by zero");
         } else {
-            System.out.println(number1/number2);
+            int division = (number1 /number2)+(number1 %number2);
+            System.out.println("Basis Division= " + division);
         }
     }
 
-    public static void getRoot(double number1) {
-        System.out.println(Math.sqrt(number1));
+    public void getRoot(double number1, double number2) {
+        if (number1>=0) {
+        double root = Math.sqrt(number1);
+        System.out.println("Basis sqrtRoot: " + number1 + "= " + root + ", " + number2 + " - if you want to know root of this number you need to enter it first");
+        } else System.out.println("Basis sqrtRoot: ERROR" + number1 + "<0");
     }
 
-    public static void getExponentiation(double number1) {
-        System.out.println(Math.pow(number1, 2));
+    public void getExponentiation(double number1, double number2) {
+        double exponent = Math.pow(number1, 2);
+        System.out.println("BasisExponentiation= " + number1 + "= " + exponent + ", " + number2 + "if you want to put of this number to squared you need to enter it first");
     }
-/*
-    public String getSum() {
-        return getSum();
-    }
-
-    public String getDifference() {
-        return getDifference();
-    }
-
-    public String getMultiplication() {
-        return getMultiplication();
-    }
-
-    public String getDivision() {
-        return getDivision();
-    }
-
-    public String getRoot() {
-        return getRoot();
-    }
-
-    public String getExponentiation() {
-        return getExponentiation();
-    }
-    */
 }
 
 
--- src/ua/hillel/HomeTask_07/MainClass.java
+++ src/ua/hillel/HomeTask_07/MainClass.java
@@ -2,22 +2,58 @@ package ua.hillel.HomeTask_07;
 
 import java.util.Scanner;
 
+import static ua.hillel.HomeTask_07.BasisCalculator.*;
+
 public class MainClass {
     public static void main(String[] args) {
-        // Scanner function = new Scanner(System.in); // creates an object of Scanner
-        // System.out.printin("Enter calculator function: ");
-        //  String name = function.nextLine(); // takes input from the keyboard
-        //  System.out.println("My name is " + name); // prints the nam
-        //  function.close(); // closes the scanner
 
-        System.out.println("Sum= " + BasisCalculator.getSum(int number1; int number2));
+        System.out.println("Enter number1: ");
+        String number1 = new Scanner(System.in).next();
+
+        System.out.println("Enter number2 or enter 0 if number2 is not need");
+        String number2 = new Scanner(System.in).next();
+
+        System.out.println("Enter one of the calculator function: +, -, *, /, ^, sqr, abs");
+        String function = new Scanner(System.in).next();
 
         BasisCalculator basis = new BasisCalculator();
-        basis.getDifference();
-        basis.getMultiplication();
-        basis.getDivision();
-        basis.getRoot();
-        basis.getExponentiation();
+        basis.number1 = Integer.parseInt(number1);
+        basis.number2 = Integer.parseInt(number2);
 
+        ScientificCalculator scientific = new ScientificCalculator();
+        scientific.number1 = Double.parseDouble(String.valueOf(number1));
+        scientific.number2 = Double.parseDouble(number2);
+
+        switch (function) {
+            case "+":
+                BasisCalculator.getSum(basis.number1, basis.number2);
+                ScientificCalculator.getSum(scientific.number1, scientific.number2);
+                break;
+            case "-":
+                BasisCalculator.getDifference(basis.number1, basis.number2);
+                ScientificCalculator.getDifference(scientific.number1, scientific.number2);
+                break;
+            case "*":
+                BasisCalculator.getMultiplication(basis.number1, basis.number2);
+                ScientificCalculator.getMultiplication(scientific.number1, scientific.number2);
+                ;
+                break;
+            case "^":
+                basis.getExponentiation(basis.number1, basis.number2);
+                scientific.getExponentiation(scientific.number1, scientific.number2);
+                break;
+            case "sqr":
+                basis.getRoot(basis.number1, basis.number2);
+                scientific.getRoot(scientific.number1, scientific.number2);
+                break;
+            case "/":
+                BasisCalculator.getDivision(basis.number1, basis.number2);
+                ScientificCalculator.getDivision(scientific.number1, scientific.number2);
+                break;
+            case "abs":
+                ScientificCalculator.getAbs(scientific.number1, scientific.number2);
+            default:
+                System.out.println("please take another calculator:) ");
+        }
     }
 }
