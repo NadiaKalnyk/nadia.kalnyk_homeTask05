84a7adb4f58bb81e5ff23a7a617d2b9d16b1afec
NadiaKalnyk <kalnuk.2016.nadiya@gmail.com>
2022-11-30 11:42:14 +0200

    the last version of the calculator:)

--- src/ua/hillel/HomeTask_07/BasisCalculator.java
+++ src/ua/hillel/HomeTask_07/BasisCalculator.java
@@ -5,24 +5,24 @@ int number1;
 int number2;
     public static void getSum(int number1, int number2) {
         int sum = number1+number2;
-        System.out.println("Basis Sum= " + sum);
+        System.out.println("Basis Sum= " + sum + "P.S: Basis works only with int numbers. If you enter fraction number - the fraction part isn't calculated :)");
     }
 
     public static void getDifference(int number1, int number2) {
         int diff = number1-number2;
-        System.out.println("Basis Difference= " + diff);
+        System.out.println("Basis Difference= " + diff + "P.S: Basis works only with int numbers. If you enter fraction number - the fraction part isn't calculated :)");
     }
 
     public static void getMultiplication(int number1, int number2) {
         int multiplication = number1 * number2;
-        System.out.println("Basis Multiplication= " + multiplication);
+        System.out.println("Basis Multiplication= " + multiplication + "P.S: Basis works only with int numbers. If you enter fraction number - the fraction part isn't calculated :)");
     }
     public static void getDivision (int number1, int number2) {
         if (number2 == 0) {
             System.out.println("ERROR.Division by zero");
         } else {
             int division = (number1 /number2)+(number1 %number2);
-            System.out.println("Basis Division= " + division);
+            System.out.println("Basis Division= " + division + "P.S: Basis works only with int numbers. If you enter fraction number - the fraction part isn't calculated :)");
         }
     }
 
@@ -30,12 +30,14 @@ int number2;
         if (number1>=0) {
         double root = Math.sqrt(number1);
         System.out.println("Basis sqrtRoot: " + number1 + "= " + root + ", " + number2 + " - if you want to know root of this number you need to enter it first");
+        System.out.println("P.S: Basis works only with int numbers. If you enter fraction number - the fraction part isn't calculated :)");
         } else System.out.println("Basis sqrtRoot: ERROR" + number1 + "<0");
     }
 
     public void getExponentiation(double number1, double number2) {
         double exponent = Math.pow(number1, 2);
         System.out.println("BasisExponentiation= " + number1 + "= " + exponent + ", " + number2 + "if you want to put of this number to squared you need to enter it first");
+        System.out.println("P.S: Basis works only with int numbers. If you enter fraction number - the fraction part isn't calculated :)");
     }
 }
 
--- src/ua/hillel/HomeTask_07/MainClass.java
+++ src/ua/hillel/HomeTask_07/MainClass.java
@@ -2,11 +2,12 @@ package ua.hillel.HomeTask_07;
 
 import java.util.Scanner;
 
+import static java.lang.String.*;
 import static ua.hillel.HomeTask_07.BasisCalculator.*;
 
 public class MainClass {
     public static void main(String[] args) {
-
+/*
         System.out.println("Enter number1: ");
         String number1 = new Scanner(System.in).next();
 
@@ -15,40 +16,44 @@ public class MainClass {
 
         System.out.println("Enter one of the calculator function: +, -, *, /, ^, sqr, abs");
         String function = new Scanner(System.in).next();
+*/
+        String function = "sqr";
+        Double number1 = Double.valueOf(valueOf(3.5));
+        Double number2 = Double.valueOf(valueOf(1.2));
 
         BasisCalculator basis = new BasisCalculator();
-        basis.number1 = Integer.parseInt(number1);
-        basis.number2 = Integer.parseInt(number2);
+        basis.number1 = (int) number1.doubleValue();
+        basis.number2 = (int) number2.doubleValue();
 
         ScientificCalculator scientific = new ScientificCalculator();
-        scientific.number1 = Double.parseDouble(String.valueOf(number1));
-        scientific.number2 = Double.parseDouble(number2);
+        scientific.number1 = Double.parseDouble(valueOf(number1));
+        scientific.number2 = Double.parseDouble(valueOf(number2));
 
         switch (function) {
             case "+":
-                BasisCalculator.getSum(basis.number1, basis.number2);
                 ScientificCalculator.getSum(scientific.number1, scientific.number2);
+                BasisCalculator.getSum(basis.number1, basis.number2);
                 break;
             case "-":
-                BasisCalculator.getDifference(basis.number1, basis.number2);
                 ScientificCalculator.getDifference(scientific.number1, scientific.number2);
+                BasisCalculator.getDifference(basis.number1, basis.number2);
                 break;
             case "*":
-                BasisCalculator.getMultiplication(basis.number1, basis.number2);
                 ScientificCalculator.getMultiplication(scientific.number1, scientific.number2);
+                BasisCalculator.getMultiplication(basis.number1, basis.number2);
                 ;
                 break;
             case "^":
-                basis.getExponentiation(basis.number1, basis.number2);
                 scientific.getExponentiation(scientific.number1, scientific.number2);
+                basis.getExponentiation(basis.number1, basis.number2);
                 break;
             case "sqr":
-                basis.getRoot(basis.number1, basis.number2);
                 scientific.getRoot(scientific.number1, scientific.number2);
+                basis.getRoot(basis.number1, basis.number2);
                 break;
             case "/":
-                BasisCalculator.getDivision(basis.number1, basis.number2);
                 ScientificCalculator.getDivision(scientific.number1, scientific.number2);
+                BasisCalculator.getDivision(basis.number1, basis.number2);
                 break;
             case "abs":
                 ScientificCalculator.getAbs(scientific.number1, scientific.number2);
