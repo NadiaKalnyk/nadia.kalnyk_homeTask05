07e6c393e077716b86493b221a0b16f367925c12
NadiaKalnyk <kalnuk.2016.nadiya@gmail.com>
2022-12-25 12:06:31 +0200

    TestNG (problem with ParametersTest)

---
+++ src/test/java/ua/hillel/tests/testNG/FailedLogin.java
@@ -0,0 +1,12 @@
+package ua.hillel.tests.testNG;
+
+import org.testng.annotations.Test;
+
+public class FailedLogin extends TestLogin {
+    @Test (dataProvider = "userData", priority = 20)
+    public void failedTest (String username, String password, String role) {
+        if (role.equals("")) {
+            throw new RuntimeException("ERROR! " + username + " hasn't role");
+        }
+    }
+}
---
+++ src/test/java/ua/hillel/tests/testNG/TestLogin.java
@@ -0,0 +1,23 @@
+package ua.hillel.tests.testNG;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.util.Arrays;
+
+public class TestLogin {
+    @DataProvider (name = "userData")
+    public Object[][] getDataFromDataProvider() {
+        return new Object[][] {
+                {"firstUser", "firstPassword", "admin"},
+                {"secondUser", "secondPassword", "member"},
+                {"thirdUser", "thirdPassword", ""},
+                {"forthUser", "thirdPassword", "guest"}
+        };
+    }
+
+    @Test (dataProvider = "userData", priority = 10)
+    public void loginTest (String username, String password, String role){
+        System.out.println("User " + username +" has role - " + role);
+    }
+}
